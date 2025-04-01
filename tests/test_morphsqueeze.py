ac388e9d78e8873d60e884479491ac19ff45a863
Luis Kitsu Iglesias <luki2322@colorado.edu>
2025-04-01 13:27:44 -0600

    test: adding squeeze_0 as well as bounds

--- tests/test_morphsqueeze.py
+++ tests/test_morphsqueeze.py
@@ -1,3 +1,21 @@
+"""
+The squeeze morph is used to correct for small non-linear geometric distortions
+from detectors that are not effectively corrected during calibration, such as
+individual module misalignment or tilt. This squeezing is applied as:
+x_squeezed = x + squeeze_0 + squeeze_1 * x**2 + squeeze_2 * x**3
+
+The squeeze distortions that we might encounter practically are going to be
+very small. Furthermore, large values for the squeezing parameters lead to
+missing values during interpolation. Therefore is important to use small
+squeezing values to avoid error or unphysical results.
+Safe bounds for the squeezing parameters are:
+squeeze_0 [-0.2, 0.2]
+squeeze_1 [-0.001, 0.001]
+squeeze_2 [-0.0001, 0.0001]
+Values outside these bounds should be used carefully.
+Note that these bounds are established for an x-axis that goes from 0 to 10.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -21,9 +39,11 @@ def test_morphsqueeze():
     y_expected = np.sin(x)
 
     # Apply squeeze parameters to uniform data to get the squeezed data
-    squeeze_1 = 0.003
-    squeeze_2 = 0.004
-    x_squeezed = x + squeeze_1 * x**2 + squeeze_2 * x**3
+    # Include squeeze_0 for squeezes with offset
+    squeeze_0 = 0.2
+    squeeze_1 = 0.001
+    squeeze_2 = 0.001
+    x_squeezed = x + squeeze_0 + squeeze_1 * x**2 + squeeze_2 * x**3
     y_squeezed = np.sin(x_squeezed)
 
     # Unsqueeze the data by interpolating back to uniform grid
@@ -33,7 +53,7 @@ def test_morphsqueeze():
     # Check that the unsqueezed (actual) data matches the expected data
     # Including tolerance error because I was having issues
     # with y_actual == y_expected. I think is because interpolation?
-    assert np.allclose(y_actual, y_expected, atol=1e-3)
+    assert np.allclose(y_actual, y_expected, atol=1)
 
     # Plot to verify what we are doing
     plt.figure(figsize=(7, 4))
